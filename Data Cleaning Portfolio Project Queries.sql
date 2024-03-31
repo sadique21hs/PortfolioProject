16878a7906bf8b4ae03d94f3653589ae79e814a3
Md Sadique Hossain <sadique21.hossain@gmail.com>
2024-04-01 01:49:11 +0600

    Update Data Cleaning Portfolio Project Queries.sql

--- Data Cleaning Portfolio Project Queries.sql
+++ Data Cleaning Portfolio Project Queries.sql
@@ -39,3 +39,19 @@ join PortfolioProject..NashvilleHousing b
   on a.ParcelID=b.ParcelID
   and a.[UniqueID ]<>b.[UniqueID ]
   where a.PropertyAddress is null
+
+
+
+
+  
+--Breaking out address in individual collums( Address, city, State)
+select PropertyAddress
+from PortfolioProject..NashvilleHousing
+----Where PropertyAddress is null
+--Order by ParcelID
+
+Select 
+SUBSTRING( PropertyAddress, 1, CHARINDEX(',', PropertyAddress)-1) as Address
+,SUBSTRING( PropertyAddress, CHARINDEX(',', PropertyAddress)+1,len(PropertyAddress)) as Address
+from PortfolioProject..NashvilleHousing
+
